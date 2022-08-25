22137c2d8b4369673994c44180a6ab18b3b1f680
Oliver Sagala <hello@sagspot.co.ke>
2022-08-25 15:57:59 +0300

    project boilerplate

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const colors = require('colors');
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const connectDB = require('./config/db');
+const schema = require('./schema/schema');
+require('dotenv').config();
+
+const app = express();
+
+connectDB();
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development',
+  })
+);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`[server]: Server is running on port ${PORT}`)
+);
